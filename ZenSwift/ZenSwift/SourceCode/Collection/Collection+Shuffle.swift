41003f61b2ed9ea5497e08e8965f5e3301ffcbc8
Alexey Roik <roland19deschain@gmail.com>
2018-03-12 01:39:40 +0300

    Add Collection+Shuffle extensions.

---
+++ ZenSwift/ZenSwift/SourceCode/Collection/Collection+Shuffle.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public extension Collection {
+    
+    /// Return a copy of 'self' with its elements shuffled.
+    var shuffle: [Iterator.Element] {
+        var list = Array(self)
+        list.shuffleInPlace()
+        return list
+    }
+    
+}
+
+public extension MutableCollection where Index == Int {
+    
+    /// Shuffle the elements of 'self' in-place.
+    mutating func shuffleInPlace() {
+        guard count > 1 else {
+            return
+        }
+        for i in startIndex ..< endIndex - 1 {
+            let j = Int.random(endIndex - i) + i
+            guard i != j else {
+                continue
+            }
+            swapAt(i, j)
+        }
+    }
+    
+}
