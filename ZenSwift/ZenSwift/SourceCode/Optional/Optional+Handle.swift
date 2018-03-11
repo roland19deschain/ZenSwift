4df99f9e9f39c3ca181ec5a6e4d9981ab850b08f
Alexey Roik <roland19deschain@gmail.com>
2018-03-12 01:29:49 +0300

    Add extensions for optional.

---
+++ ZenSwift/ZenSwift/SourceCode/Optional/Optional+Handle.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public extension Optional {
+    
+    /// Executes the closure if there is some value.
+    @discardableResult func ifSome(_ handler: (Wrapped)->()) -> Optional {
+        switch self {
+        case .none:                 break
+        case .some(let wrapped):    handler(wrapped)
+        }
+        return self
+    }
+    
+    /// Executes the closure if there is no value.
+    @discardableResult func ifNone(_ handler: ()->()) -> Optional {
+        switch self {
+        case .none: handler()
+        case .some: break
+        }
+        return self
+    }
+    
+}
