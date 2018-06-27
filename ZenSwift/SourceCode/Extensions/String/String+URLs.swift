dd693b92c3143a1143baec394eee1b3db57b3373
Alexey Roik <roland19deschain@gmail.com>
2018-06-28 00:32:16 +0300

    Add 'String' extensions for urls detection.

--- ZenSwift/SourceCode/Extensions/String/String+URLs.swift
+++ ZenSwift/SourceCode/Extensions/String/String+URLs.swift
@@ -21,4 +21,52 @@ public extension String {
         return url
     }
     
+    /// Returns an array of detected urls
+    var detectedURLs: [URL] {
+        let type = NSTextCheckingResult.CheckingType.link.rawValue
+        guard let detector = try? NSDataDetector(types: type) else {
+            return []
+        }
+        let range = NSRange(location: 0, length: count)
+        return detector
+            .matches(in: self,
+                     options: [],
+                     range: range)
+            .compactMap {
+                return $0.resultType == NSTextCheckingResult.CheckingType.link
+                    ? $0.url
+                    : nil
+        }
+    }
+    
+    /**
+     Enumerates an array of detected urls. Process may be interrupted by
+     setting pointer to boolean value (stop flag) to true.
+     
+     For Example:
+     ```
+     text.detectURLs { url, stop in
+         if url == needle {
+             // stop this loop
+             stop.pointee = true
+         }
+     }
+     ```
+     */
+    func detectURLs(handler: @escaping (URL, UnsafeMutablePointer<ObjCBool>) -> Void) {
+        let checkingType = NSTextCheckingResult.CheckingType.link
+        let range = NSRange(location: 0, length: count)
+        (try? NSDataDetector(types: checkingType.rawValue))?
+            .enumerateMatches(in: self,
+                              options: [],
+                              range: range) { result, flags, stop in
+                                guard let result = result,
+                                    let url = result.url,
+                                    result.resultType == checkingType else {
+                                        return
+                                }
+                                handler(url, stop)
+        }
+    }
+    
 }
