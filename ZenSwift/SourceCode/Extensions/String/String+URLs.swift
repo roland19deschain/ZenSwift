059f8f6d06c79932067b14206fc5d7bc83903d18
Alexey Roik <roland19deschain@gmail.com>
2018-06-28 23:59:19 +0300

    Enable url detection extensions for String.

--- ZenSwift/SourceCode/Extensions/String/String+URLs.swift
+++ ZenSwift/SourceCode/Extensions/String/String+URLs.swift
@@ -22,20 +22,20 @@ public extension String {
     }
     
     /// Returns an array of detected urls
-//    var detectedURLs: [URL] {
-//        let checkingType = NSTextCheckingResult.CheckingType.link
-//        guard let detector = try? NSDataDetector(types: checkingType.rawValue) else {
-//            return []
-//        }
-//        let range = NSRange(location: 0, length: count)
-//        return detector
-//            .matches(in: self,
-//                     options: [],
-//                     range: range)
-//            .compactMap {
-//                $0.resultType == checkingType ? $0.url : nil
-//        }
-//    }
+    var detectedURLs: [URL] {
+        let checkingType = NSTextCheckingResult.CheckingType.link
+        guard let detector = try? NSDataDetector(types: checkingType.rawValue) else {
+            return []
+        }
+        let range = NSRange(location: 0, length: count)
+        return detector
+            .matches(in: self,
+                     options: [],
+                     range: range)
+            .compactMap {
+                $0.resultType == checkingType ? $0.url : nil
+        }
+    }
     
     /**
      Enumerates an array of detected urls. Process may be interrupted by
@@ -51,20 +51,20 @@ public extension String {
      }
      ```
      */
-//    func detectURLs(handler: @escaping (URL, UnsafeMutablePointer<ObjCBool>) -> Void) {
-//        let checkingType = NSTextCheckingResult.CheckingType.link
-//        let range = NSRange(location: 0, length: count)
-//        (try? NSDataDetector(types: checkingType.rawValue))?
-//            .enumerateMatches(in: self,
-//                              options: [],
-//                              range: range) { result, flags, stop in
-//                                guard let result = result,
-//                                    let url = result.url,
-//                                    result.resultType == checkingType else {
-//                                        return
-//                                }
-//                                handler(url, stop)
-//        }
-//    }
+    func detectURLs(handler: @escaping (URL, UnsafeMutablePointer<ObjCBool>) -> Void) {
+        let checkingType = NSTextCheckingResult.CheckingType.link
+        let range = NSRange(location: 0, length: count)
+        (try? NSDataDetector(types: checkingType.rawValue))?
+            .enumerateMatches(in: self,
+                              options: [],
+                              range: range) { result, flags, stop in
+                                guard let result = result,
+                                    let url = result.url,
+                                    result.resultType == checkingType else {
+                                        return
+                                }
+                                handler(url, stop)
+        }
+    }
     
 }
