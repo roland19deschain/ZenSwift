f4f8887bfeffe0ffcbd385bd429aaed3d1d13fc8
Alexey Roik <roland19deschain@gmail.com>
2018-11-03 23:01:51 +0300

    Add convenient extensions for json serialization to 'Encodable' and 'Decodable' protocols.

---
+++ ZenSwift/SourceCode/Extensions/Encodable/Encodable+JSON.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public extension Encodable {
+    
+    /// Returns a JSON-encoded representation of self.
+    var json: String? {
+        guard let data = try? JSONEncoder().encode(self) else {
+            return nil
+        }
+        return String(data: data,
+                      encoding: .utf8)
+    }
+    
+}
