11ca9ba5021efc7e77d6d249f7b092b0a6b77d65
Alexey Roik <roland19deschain@gmail.com>
2019-02-15 02:21:13 +0300

    Add 'Mirror' extensions.

---
+++ ZenSwift/SourceCode/Extensions/Mirror/Mirror+ReflectProperties.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public extension Mirror {
+    
+    /// Iterate through all of properties in the reflected subject,
+    /// and perform the same action on them.
+    /// - Parameters:
+    ///     - matchingType: The type of properties in reflecting subject which are involved.
+    ///     - recursively: A Boolean value indicating whether an iteration occurs recursively.
+    ///     - closure: A closure that will be apply to every property of reflecting subject.
+    
+    func reflectProperties<T>(
+        matchingType type: T.Type = T.self,
+        recursively: Bool = false,
+        using closure: (T) -> Void
+        ) {
+        for child in children {
+            (child.value as? T).map(closure)
+            
+            if recursively {
+                Mirror(reflecting: child.value)
+                    .reflectProperties(recursively: true,
+                                       using:closure)
+            }
+        }
+    }
+    
+}
