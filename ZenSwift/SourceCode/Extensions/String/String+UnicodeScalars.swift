b2bb5db683be306471f421fe4ed38ecb201073ec
Alexey Roik <roland19deschain@gmail.com>
2019-09-01 22:19:52 +0300

    Add 'String+UnicodeScalars' extension.

---
+++ ZenSwift/SourceCode/Extensions/String/String+UnicodeScalars.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension String {
+    
+    /**
+     Creates an instance initialized by the sequence of unicodeScalars.
+     - Parameter unicodeScalars: The sequence of unicodeScalars.
+     */
+    init<S: Sequence>(
+        _ unicodeScalars: S
+        ) where S.Iterator.Element == UnicodeScalar {
+        self = String(UnicodeScalarView(unicodeScalars))
+    }
+    
+}
