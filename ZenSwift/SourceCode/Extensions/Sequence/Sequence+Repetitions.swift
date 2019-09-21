4f5ba427d604d8c0bdd3e244029b2491cbc7b56b
Alexey Roik <roland19deschain@gmail.com>
2019-09-22 01:39:15 +0300

    Improve naming in 'Sequence+Repetitions' extension.

--- ZenSwift/SourceCode/Extensions/Sequence/Sequence+Repetitions.swift
+++ ZenSwift/SourceCode/Extensions/Sequence/Sequence+Repetitions.swift
@@ -5,7 +5,7 @@ public extension Sequence where Element: Hashable {
     /// Returns a new dictionary containing the key-value pairs,
     /// in which the key is a sequence element,
     /// and the value is the number of repetitions of this element in the sequence.
-    var elementRepetitionsMap: [Element: Int] {
+    var repetitionsMap: [Element: Int] {
         return Dictionary(
             map { ($0, 1) },
             uniquingKeysWith: +
@@ -13,8 +13,8 @@ public extension Sequence where Element: Hashable {
     }
     
     /// Returns the elements of a sequence with the same and maximum number of repetitions.
-    var mostRepeatingElements: [Element] {
-        let repetitionsMap = elementRepetitionsMap
+    var mostRepeating: [Element] {
+        let repetitionsMap = self.repetitionsMap
         guard let maxValue = repetitionsMap.values.max() else {
             return []
         }
