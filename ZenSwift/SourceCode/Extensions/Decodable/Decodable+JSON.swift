657b86ab506855b0702c7202a8d04a28b26bcfb9
Alexey Roik <roland19deschain@gmail.com>
2020-05-18 01:54:02 +0300

    Add 'Encodable' protocol extensions

--- ZenSwift/SourceCode/Extensions/Decodable/Decodable+JSON.swift
+++ ZenSwift/SourceCode/Extensions/Decodable/Decodable+JSON.swift
@@ -3,9 +3,11 @@ import Foundation
 public extension Decodable {
 	
 	/// Creates a new instance by decoding from the given JSON string.
-	init?(json: String) {
-		guard let value = try? JSONDecoder()
-			.decode(Self.self, from: Data(json.utf8)) else {
+	init?(jsonRepresentation: String) {
+		guard let value = try? JSONDecoder().decode(
+			Self.self,
+			from: Data(jsonRepresentation.utf8)
+			) else {
 				return nil
 		}
 		self = value
--- ZenSwift/SourceCode/Extensions/Encodable/Encodable+JSON.swift
+++ ZenSwift/SourceCode/Extensions/Encodable/Encodable+JSON.swift
@@ -3,7 +3,7 @@ import Foundation
 public extension Encodable {
 	
 	/// Returns a JSON-encoded representation of self.
-	var json: String? {
+	var jsonRepresentation: String? {
 		guard let data = try? JSONEncoder().encode(self) else {
 			return nil
 		}
@@ -11,6 +11,17 @@ public extension Encodable {
 			data: data,
 			encoding: .utf8
 		)
+	}
+	
+	/// Returns dictionary representation of self.
+	var dictionaryRepresentation: [String: Any] {
+		guard let data = try? JSONEncoder().encode(self),
+			let dictionary =  try? JSONSerialization.jsonObject(
+				with: data
+				) as? [String: Any] else {
+					return [:]
+		}
+		return dictionary
 	}
 	
 }
