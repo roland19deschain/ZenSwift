b4978b4a397e6f358d472f766302cb3aae46926e
Alexey Roik <roland19deschain@gmail.com>
2021-04-25 20:00:53 +0300

    Update 'Package.swift'

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
-let package = Package(name: "ZenSwift")
+let package = Package(
+	name: "ZenSwift",
+	platforms: [
+		.macOS(.v10_10),
+		.iOS(.v9),
+		.tvOS(.v9),
+		.watchOS(.v2)
+	],
+	products: [
+		.library(name: "ZenSwift", targets: ["ZenSwift"])
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "ZenSwift", dependencies: [])
+	]
+)
