a2c71a78180bc123d0ded0a67b1f28c2d7b2d292
Alexey Roik <roland19deschain@gmail.com>
2021-04-25 20:28:00 +0300

    Update product names

--- ZenSwift/SupportingFiles/ZenSwift.h
+++ ZenSwift/SupportingFiles/ZenSwift.h
@@ -1,3 +1,13 @@
+#import <Foundation/Foundation.h>
+#import "TargetConditionals.h"
+
+#if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
+#elif TARGET_OS_MAC
+#import <Cocoa/Cocoa.h>
+#elif TARGET_OS_WATCH
+#import <WatchKit/WatchKit.h>
+#endif
+
 FOUNDATION_EXPORT double ZenSwiftVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZenSwiftVersionString[];
