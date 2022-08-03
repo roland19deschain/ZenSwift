5c1832f1054772f99c8d353996178271b563142c
Alexey Roik <roland19deschain@gmail.com>
2022-08-04 02:39:29 +0300

    Add new string extension (version compare), update version to '2.1.6'

--- Sources/Extensions/String/String+Compare.swift
+++ Sources/Extensions/String/String+Compare.swift
@@ -38,4 +38,46 @@ public extension String {
 		) == .orderedSame
 	}
 	
+	/**
+	 Returns the `ComparisonResult` of comparing two versions strings.
+	 
+	 This method will make sure we two version strings got an identical period
+	 and the same number of digit before making the comparison.
+	 
+	 Usage example:
+	 ```
+	 "1.0.0".versionCompare("1.0.0") // .orderedSame
+	 "0.0.2".versionCompare("0.0.1") // .orderedDescending
+	 "1.0.0".versionCompare("1.1")   // .orderedAscending
+	 ```
+	 - parameter otherVersion: The version string to compare.
+	 */
+	func versionCompare(
+		_ otherVersion: String,
+		delimiter: String = "."
+	) -> ComparisonResult {
+		var versionComponents: [String] = components(separatedBy: delimiter)
+		var otherVersionComponents: [String] = otherVersion.components(separatedBy: delimiter)
+		let lengthDiff: Int = versionComponents.count - otherVersionComponents.count
+		
+		let a = compare("")
+
+		if lengthDiff == 0 {
+			return compare(otherVersion, options: .numeric)
+		} else {
+			let zeros: [String] = Array(
+				repeating: "0",
+				count: abs(lengthDiff)
+			)
+			if lengthDiff > 0 {
+				otherVersionComponents.append(contentsOf: zeros)
+			} else {
+				versionComponents.append(contentsOf: zeros)
+			}
+			let lhsVersion: String = versionComponents.joined(separator: delimiter)
+			let rhsVersion: String = otherVersionComponents.joined(separator: delimiter)
+			return lhsVersion.compare(rhsVersion, options: .numeric)
+		}
+	}
+	
 }
